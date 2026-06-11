69bac673975f7ce6034e64e90e5612ccc6ffe2cb
Rajvi <rajvixx123@gmail.com>
2026-06-11 22:58:47 +0200

    footer and header css but also working buttons and made aboutme tekst

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,14 +12,27 @@ export default function AboutPage() {
                 transition={{ duration: 0.6 }}
             >
                 <div className="about-text">
-                    <h1>About Me</h1>
+                    <h1>Hello Reader,</h1>
 
                     <p>
-                        Hi my name is Rajvi Fatingan and ....
+                        I’m Rajvi Fatingan — a developer who likes building cool, fun but also usefull things on a web
+                        application.
                     </p>
 
                     <p>
-                        {/* extra text */}
+                        I mainly work with Angular, React/next.js and Spring Boot Java.
+                        I’m focused on improving my skills and gaining more knowledge by building real projects
+                        and making them better over time.
+                    </p>
+
+                    <p>
+                        I’m the kind of developer who learns by building. I like experimenting, and improving the codes
+                        until everything feels right and smooth to use.
+                    </p>
+
+                    <p>
+                        I like a clean design, to share creative ideas, and turning random thoughts into functional and enjoyable projects.
+                        i also love to make worthy project that could help others, think about education or translation or just something handy like in the pocket wardrobe.
                     </p>
                 </div>
 
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,24 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <p></p>
-
             <a
                 href="https://www.linkedin.com/in/rajvi-fatingan-32a879391/"
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                LinkedIn
+                <img src="/images/linkedin.png" alt="LinkedIn" />
+            </a>
+
+            <a
+                href="https://github.com/rajviSF1207"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src="/images/github.png" alt="GitHub" />
+            </a>
+
+            <a href="mailto:rajvifatingan04@gmail.com">
+                <img src="/images/mail.png" alt="Email" />
             </a>
         </footer>
     );
